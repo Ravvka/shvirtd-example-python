4298015e6e19f3bb05b11bb02b9f4d6f7d1dcebf
Ravil Minibaev <exhaustedmr@gmail.com>
2026-07-03 22:10:21 +0300

    add sh

---
+++ load-build-deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+REPO_URL="https://github.com/Ravvka/shvirtd-example-python.git"
+TARGET_PATH="/opt/net-05-task-04-05"
+
+
+if [ -d "$TARGET_PATH" ]; then
+  cd "$TARGET_PATH"
+  git pull
+else
+  sudo git clone "$REPO_URL" "$TARGET_PATH"
+  sudo chown -R $USER:$USER "$TARGET_PATH"
+  cd "$TARGET_PATH" && git pull
+fi
+
+docker compose down
+docker compose up -d --build --no-cache
