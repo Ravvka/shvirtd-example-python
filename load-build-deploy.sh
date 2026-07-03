8b5000b0927c820cb7d49439be7d1a9386c344d4
Ravil Minibaev <exhaustedmr@gmail.com>
2026-07-03 22:31:25 +0300

    last upd sh

--- load-build-deploy.sh
+++ load-build-deploy.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-
+# sudo sh load-build-deploy.sh
 REPO_URL="https://github.com/Ravvka/shvirtd-example-python.git"
 TARGET_PATH="/opt/net-05-task-04-05"
-
 
 if [ -d "$TARGET_PATH" ]; then
   cd "$TARGET_PATH"
   git pull
 else
-  sudo git clone "$REPO_URL" "$TARGET_PATH"
-  sudo chown -R $USER:$USER "$TARGET_PATH"
+  git clone "$REPO_URL" "$TARGET_PATH"
+  chown -R $USER:$USER "$TARGET_PATH"
   cd "$TARGET_PATH" && git pull
 fi
 
 docker compose down
-docker compose up -d --build --no-cache
+docker compose up -d --build
